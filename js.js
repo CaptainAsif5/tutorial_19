f4257df291ed991822697f0b86e2a41eb38f6596
asif khan <asifkhandeveloper4@gmail.com>
2023-09-17 14:21:36 +0500

    this is a getting data for variables

---
+++ js.js
@@ -0,0 +1,10 @@
+var table = document.getElementById("tbody");
+var name = "abc";
+var age = "18";
+var classes = "08";
+table.innerHTML +=
+    `<tr>
+<td>${name}</td>
+<td>${age}</td>
+<td>${classes}</td>
+</tr>`;
